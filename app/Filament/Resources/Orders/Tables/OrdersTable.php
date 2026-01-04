180ce81bf0b0e71436944df5b70ee4fd3e90bec1
Daniel Thomann <g.danieltho@gmail.com>
2026-01-03 23:39:01 -0300

    feat: agrego fecha productos

---
+++ app/Filament/Resources/Orders/Tables/OrdersTable.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Filament\Resources\Orders\Tables;
+
+use Filament\Actions\BulkActionGroup;
+use Filament\Actions\DeleteBulkAction;
+use Filament\Actions\EditAction;
+use Filament\Tables\Columns\Summarizers\Sum;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+
+class OrdersTable
+{
+    public static function configure(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('user.name')
+                    ->searchable(),
+                TextColumn::make('product.name')
+                    ->searchable(),
+                TextColumn::make('price')
+                    ->money('ARS', 100)
+                    ->summarize(Sum::make()->money('ARS',100))
+                    ->sortable(),
+                TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->defaultGroup('product.name')
+            ->filters([
+                //
+            ])
+            ->recordActions([
+                EditAction::make(),
+            ])
+            ->toolbarActions([
+                BulkActionGroup::make([
+                    DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+}
